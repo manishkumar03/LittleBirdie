c79055c3513e76ad22057bc5fc353c6a8e7530cc
Manish Kumar <manish.bansal@gmail.com>
2021-05-29 23:27:06 -0400

    Add dummy Twitter API keys

---
+++ LittleBirdie/NetworkingLayer/TwitterAPIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterAPIKeys.swift
+//  LittleBirdie
+//
+//  Created by Manish Kumar on 2021-04-03.
+//
+
+import Foundation
+
+/// These are dummy keys. Please provide your own valid keys to successfully run the app.
+let TWITTER_CONSUMER_KEY = "5FTJHK890778QC03ht3434dOaTssVhUFYOdfdsfssf"
+let TWITTER_CONSUMER_SECRET = "c3AbjJKJLoul8897038OtW5qkfgfdgdgdgqSEKIZa0jlHsgF8kBngr31"
+let TWITTER_URL_SCHEME = "abcdefghdij"
