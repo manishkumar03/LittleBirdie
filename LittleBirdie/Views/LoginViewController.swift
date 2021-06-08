2ea4f58f4ee32321a27cb96b8bcbd1f7558c2197
Manish Kumar <mkumar@acvauctions.com>
2021-06-07 21:50:34 -0400

    Add gradients to login page

--- LittleBirdie/Views/LoginViewController.swift
+++ LittleBirdie/Views/LoginViewController.swift
@@ -17,6 +17,11 @@ class LoginViewController: UIViewController, ASWebAuthenticationPresentationCont
         setupBindings()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.setGradientBackground()
+    }
+
     func setupBindings() {
         viewModel.requestTokenReceived.bind { tokenReceived in
             if tokenReceived {
@@ -53,6 +58,18 @@ class LoginViewController: UIViewController, ASWebAuthenticationPresentationCont
 
     func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
         return view.window!
+    }
+
+    func setGradientBackground() {
+        let colorTop =  UIColor(red: 193.0/255.0, green: 101.0/255.0, blue: 221.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 92.0/255.0, green: 39.0/255.0, blue: 254.0/255.0, alpha: 1.0).cgColor
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+
+        self.view.layer.insertSublayer(gradientLayer, at:0)
     }
 }
 
